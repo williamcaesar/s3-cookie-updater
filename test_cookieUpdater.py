ca17ced4c36ea16f4d5f403916e803179b9927d4
Caesar <williamcaesar@protonmail.com>
2019-04-18 15:05:22 -0300

    Update test_cookieUpdater.py

--- test_cookieUpdater.py
+++ test_cookieUpdater.py
@@ -5,4 +5,4 @@ from updater import CookieUpdater
 class TestCookieUpdater(TestCase):
     def test___update_local(self):
         updater = CookieUpdater()
-        updater.update_local('/home/caesar/')
+        updater.update_local('.')
